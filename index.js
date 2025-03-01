e1c4355be0432d0db7c032db684e9b9d6e771833
WB-Painel <126594701+WB-Painel@users.noreply.github.com>
2025-03-01 12:27:32 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const require = createRequire(import.meta.url);
+
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", function (req, res) {
+
+res.send("Hello, Sinapsel Factory");
+
+});
+
+app.listen(port, () => {
+
+console.info("Application running on http://localhost:3000");
+
+});
