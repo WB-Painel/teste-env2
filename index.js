a4ff05b6c1a62ad19f65650ea60508be6e669399
WB-Painel <126594701+WB-Painel@users.noreply.github.com>
2025-09-20 11:46:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ const now = new Date();
 
 const nextRun = new Date();
 
-nextRun.setHours(targetHour, 46, 0, 0);
+nextRun.setHours(targetHour, 50, 0, 0);
 
 if (now >= nextRun) {
 
@@ -116,7 +116,7 @@ nextRun.setDate(nextRun.getDate() + 1);
 
 const msUntilNextRun = nextRun - now;
 
-//console.log(⏳ Aguardando ${(msUntilNextRun / 1000 / 60).toFixed(2)} minutos até a primeira execução às ${targetHour}:00);
+console.log(`⏳ Aguardando ${(msUntilNextRun / 1000 / 60).toFixed(2)} minutos até a primeira execução às ${targetHour}:00`);
 
 setTimeout(() => {
 
